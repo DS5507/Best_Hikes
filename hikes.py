122500c1597b24f766d3bc9b2e29bf53abd2709c
DS5507 <50781786+DS5507@users.noreply.github.com>
2019-06-19 20:38:57 -0400

    Pulls lat/lon from address

--- hikes.py
+++ hikes.py
@@ -1,7 +1,15 @@
 # hikes.py
 
-import requests
+import csv
+import json
+import os
 
+
+#from dotenv import load_dotenv
+import requests
+import datetime
+
+#load_dotenv()
 
 ## User Input
 print("Hey, we're stoked that you want to go hiking.  Let's find you the best route!")
@@ -10,7 +18,7 @@ print("We'll need your basecamp location to find your best route.")
 street_raw = input("What's your street address? (Ex: 123 Main St): ").lower()
 street = street_raw.replace(" ", "+")
 zipcode = input("How about your zip code? (Ex: 10013): ").lower()
-distance = input("Just how many miles would you be willing to travel to find the best route?").lower()
+distance = input("Just how many miles would you be willing to travel to find the best route? ").lower()
 
 # TODO: Add Address Validation
 # TODO: Add Additional Options
@@ -18,8 +26,18 @@ distance = input("Just how many miles would you be willing to travel to find the
 ## Address to Lat/Lon
 
 address_url = f"https://geocoding.geo.census.gov/geocoder/locations/address?street={street}&zip={zipcode}&benchmark=Public_AR_Current&format=json"
-print(address_url)
+response = requests.get(address_url)
+parsed_response = json.loads(response.text)
+
+matched_address = parsed_response['result']['addressMatches'][0]['matchedAddress']
+
+coordinates = parsed_response['result']['addressMatches'][0]['coordinates']
+lat = parsed_response['result']['addressMatches'][0]['coordinates']['y'] 
+lon = parsed_response['result']['addressMatches'][0]['coordinates']['x']
+
+
 breakpoint()
+
 
 
 ## Lat/Lon to Route
