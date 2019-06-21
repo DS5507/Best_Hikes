6a85234e12fb54bb8cfc1ef4854ce4186f1c2b1a
DS5507 <50781786+DS5507@users.noreply.github.com>
2019-06-21 17:15:25 -0400

    Added url to results and cleaned up output

--- hikes.py
+++ hikes.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 hike_key = os.environ.get("hike_key")
 
-print("Hey, we're stoked that you want to go hiking.  Let's find you the best route!")
+print("Hey, I'm stoked that you want to go hiking.  Let's find you the best route!")
 print("--------------------")
 
 while True:
@@ -23,8 +23,8 @@ while True:
     street_raw = input("What's your street address? (Ex: 123 Main St): ").lower()
     street = street_raw.replace(" ", "+")
     zipcode = input("How about your zip code? (Ex: 10013): ").lower()
-
-
+    print("Let me check my map and compass and see if I can find you...")
+    print("--------------------")
 
     # TODO: Add Additional Options
 
@@ -32,15 +32,22 @@ while True:
     address_url = f"https://geocoding.geo.census.gov/geocoder/locations/address?street={street}&zip={zipcode}&benchmark=Public_AR_Current&format=json"
     response_address = requests.get(address_url)
     parsed_response_address = json.loads(response_address.text)
-
+    
     while parsed_response_address['result']['addressMatches'] == []:
         print("There's a little problem... we couldn't find your address.  Why don't you try again?  Pro Tip:  We just need your street address and your zip code, not the city or state.")
         break
     else:
-        print("Great, we found your starting point!")
+        print("Great, we found your starting point!")  
         break
 
 distance = input("Just how many miles would you be willing to travel to find the best route? ").lower()
+if float(distance) >= 201:
+    print("Sorry, I can only look 200 miles in any direction.  I'll go ahead and look 200 miles out for you though!")
+    distance = 200
+else:
+    pass
+
+print("Okay, just a sec while we find something awesome for you.")
 
 matched_address = parsed_response_address['result']['addressMatches'][0]['matchedAddress']
 coordinates = parsed_response_address['result']['addressMatches'][0]['coordinates']
@@ -48,15 +55,17 @@ lat = parsed_response_address['result']['addressMatches'][0]['coordinates']['y']
 lon = parsed_response_address['result']['addressMatches'][0]['coordinates']['x']
 
 ## Lat/Lon to Route
-hike_url = f"https://www.hikingproject.com/data/get-trails?lat={lat}&lon={lon}&minStars=3&sort=quality&maxDistance={distance}&key={hike_key}"
+hike_url = f"https://www.hikingproject.com/data/get-trails?lat={lat}&lon={lon}&minStars=3&sort=quality&maxDistance={distance}&maxResults=500&key={hike_key}"
 response_hike = requests.get(hike_url)
 parsed_response_hike = json.loads(response_hike.text)
 hike_name = (parsed_response_hike)['trails'][0]['name']
-hike_summary = (parsed_response_hike)['trails'][0]['name']
+hike_summary = (parsed_response_hike)['trails'][0]['summary']
 hike_length = (parsed_response_hike)['trails'][0]['length']
 hike_rating = (parsed_response_hike)['trails'][0]['stars']
 hike_difficulty_raw = (parsed_response_hike)['trails'][0]['difficulty']
 hike_location = (parsed_response_hike)['trails'][0]['location']
+hike_url_response = (parsed_response_hike)['trails'][0]['url']
+
 
 if hike_difficulty_raw == "green":
     hike_difficulty = "Easy"
@@ -68,4 +77,10 @@ print("--------------------")
 print(f"We found a great hike for you! The highest rated route within {distance} miles is named '{hike_name}'.")
 print(f"The route is {hike_length} miles long and is located in {hike_location}.")
 print(f"The community has given this trail {hike_rating} out of 5 stars and labeled its diffulty as {hike_difficulty}.")
-print(f"Here's a summary of the trail: {hike_summary}.")
+print(f"Here's a summary of the trail: {hike_summary}")
+print(f"You can check out more about this hike at {hike_url_response}.")
+breakpoint()
+
+
+
+# TODO: What is no trails?
