f32e7ca7a304ee2394f41f73cf4c47164616b62c
DS5507 <50781786+DS5507@users.noreply.github.com>
2019-06-21 15:24:03 -0400

    Sucessfully pulls info from both APIs

--- hikes.py
+++ hikes.py
@@ -5,11 +5,14 @@ import json
 import os
 
 
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 import requests
 import datetime
 
-#load_dotenv()
+load_dotenv()
+
+hike_key = os.environ.get("hike_key")
+
 
 ## User Input
 print("Hey, we're stoked that you want to go hiking.  Let's find you the best route!")
@@ -24,20 +27,20 @@ distance = input("Just how many miles would you be willing to travel to find the
 # TODO: Add Additional Options
 
 ## Address to Lat/Lon
-
 address_url = f"https://geocoding.geo.census.gov/geocoder/locations/address?street={street}&zip={zipcode}&benchmark=Public_AR_Current&format=json"
-response = requests.get(address_url)
-parsed_response = json.loads(response.text)
+response_address = requests.get(address_url)
+parsed_response_address = json.loads(response_address.text)
 
-matched_address = parsed_response['result']['addressMatches'][0]['matchedAddress']
-
-coordinates = parsed_response['result']['addressMatches'][0]['coordinates']
-lat = parsed_response['result']['addressMatches'][0]['coordinates']['y'] 
-lon = parsed_response['result']['addressMatches'][0]['coordinates']['x']
-
-
-breakpoint()
-
-
+matched_address = parsed_response_address['result']['addressMatches'][0]['matchedAddress']
+coordinates = parsed_response_address['result']['addressMatches'][0]['coordinates']
+lat = parsed_response_address['result']['addressMatches'][0]['coordinates']['y'] 
+lon = parsed_response_address['result']['addressMatches'][0]['coordinates']['x']
 
 ## Lat/Lon to Route
+hike_url = f"https://www.hikingproject.com/data/get-trails?lat={lat}&lon={lon}&minStars=3&sort=quality&maxDistance={distance}&key={hike_key}"
+response_hike = requests.get(hike_url)
+parsed_response_hike = json.loads(response_hike.text)
+hike_name = (parsed_response_hike)['trails'][0]['name']
+print(f"We found a great hike for you named '{hike_name}''")
+
+breakpoint()
