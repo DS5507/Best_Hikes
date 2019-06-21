c7f14f1e7682094d405818bfcab48e4e4695ecb8
DS5507 <50781786+DS5507@users.noreply.github.com>
2019-06-21 16:43:22 -0400

    Created while loop to validate address

--- hikes.py
+++ hikes.py
@@ -13,23 +13,34 @@ load_dotenv()
 
 hike_key = os.environ.get("hike_key")
 
-
-## User Input
 print("Hey, we're stoked that you want to go hiking.  Let's find you the best route!")
 print("--------------------")
-print("We'll need your basecamp location to find your best route.")
-street_raw = input("What's your street address? (Ex: 123 Main St): ").lower()
-street = street_raw.replace(" ", "+")
-zipcode = input("How about your zip code? (Ex: 10013): ").lower()
+
+while True:
+    ## User Input
+
+    print("We'll need your basecamp location to find your best route.")
+    street_raw = input("What's your street address? (Ex: 123 Main St): ").lower()
+    street = street_raw.replace(" ", "+")
+    zipcode = input("How about your zip code? (Ex: 10013): ").lower()
+
+
+
+    # TODO: Add Additional Options
+
+    ## Address to Lat/Lon
+    address_url = f"https://geocoding.geo.census.gov/geocoder/locations/address?street={street}&zip={zipcode}&benchmark=Public_AR_Current&format=json"
+    response_address = requests.get(address_url)
+    parsed_response_address = json.loads(response_address.text)
+
+    while parsed_response_address['result']['addressMatches'] == []:
+        print("There's a little problem... we couldn't find your address.  Why don't you try again?  Pro Tip:  We just need your street address and your zip code, not the city or state.")
+        break
+    else:
+        print("Great, we found your starting point!")
+        break
+
 distance = input("Just how many miles would you be willing to travel to find the best route? ").lower()
-
-# TODO: Add Address Validation
-# TODO: Add Additional Options
-
-## Address to Lat/Lon
-address_url = f"https://geocoding.geo.census.gov/geocoder/locations/address?street={street}&zip={zipcode}&benchmark=Public_AR_Current&format=json"
-response_address = requests.get(address_url)
-parsed_response_address = json.loads(response_address.text)
 
 matched_address = parsed_response_address['result']['addressMatches'][0]['matchedAddress']
 coordinates = parsed_response_address['result']['addressMatches'][0]['coordinates']
@@ -50,7 +61,7 @@ hike_location = (parsed_response_hike)['trails'][0]['location']
 if hike_difficulty_raw == "green":
     hike_difficulty = "Easy"
 elif hike_difficulty_raw == "Black":
-    hike_difficulty = "Difficult"
+    hike_difficulty = "Hard"
 else:
     hike_difficulty = "Moderate"
 print("--------------------")
