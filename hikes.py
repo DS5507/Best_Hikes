5c11a26af6ee94393d9f963ea54e7727e3485f4a
DS5507 <50781786+DS5507@users.noreply.github.com>
2019-06-23 15:23:12 -0400

    Fixed difficulty loop, added loop if no results found, added loop if no positive response from HP API

--- hikes.py
+++ hikes.py
@@ -38,137 +38,150 @@ while True:
         print("--------------------")
         break
     else:
-        print("Great, we found your starting point!")  
+        print(f"Great, I found your starting point!  I'll start my search from: {parsed_response_address['result']['addressMatches'][0]['matchedAddress']}")  
+        print("")
         break
 
-how_hard = input("On a scale of 1-5, what difficulty level are you interested in? ").lower()
-while how_hard not in str([1, 2, 3, 4, 5]):
-    print("Can you pick something between 1 - 5?")
+
+while True:
+    
+    how_hard = input("On a scale of 1-5, what difficulty level are you interested in? ").lower()
+
+    while how_hard not in str([1, 2, 3, 4, 5]):
+        print("Can you pick something between 1 - 5?")
+        print("--------------------")
+        how_hard = input("On a scale of 1-5, what difficulty level are you interested in? ").lower()
+    else:
+        print(f"Okay, I'll look for something with a difficulty rating of {how_hard} out of 5")
+        print("")
+
+ 
+    distance = input("Just how many miles would you be willing to travel to find the best route? ").lower()
+    if float(distance) >= 201:
+        print("Sorry, I can only look 200 miles in any direction.  I'll go ahead and look 200 miles out for you though!")
+        distance = 200
+
+    print("Okay, just a sec while I try to find something awesome for you.")
     print("--------------------")
-    break
-else:
-    print(f"Okay, I'll look for something rated a '{how_hard}'")
-
-distance = input("Just how many miles would you be willing to travel to find the best route? ").lower()
-if float(distance) >= 201:
-    print("Sorry, I can only look 200 miles in any direction.  I'll go ahead and look 200 miles out for you though!")
-    distance = 200
-else:
-    pass
+    print("")
 
 
+    matched_address = parsed_response_address['result']['addressMatches'][0]['matchedAddress']
+    coordinates = parsed_response_address['result']['addressMatches'][0]['coordinates']
+    lat = parsed_response_address['result']['addressMatches'][0]['coordinates']['y'] 
+    lon = parsed_response_address['result']['addressMatches'][0]['coordinates']['x']
 
-print("Okay, just a sec while we find something awesome for you.")
-print("--------------------")
+    ## Lat/Lon to Route
+    hike_url = f"https://www.hikingproject.com/data/get-trails?lat={lat}&lon={lon}&minStars=3&sort=quality&maxDistance={distance}&maxResults=500&key={hike_key}"
+    response_hike = requests.get(hike_url)
+    parsed_response_hike = json.loads(response_hike.text)
+    
 
-
-matched_address = parsed_response_address['result']['addressMatches'][0]['matchedAddress']
-coordinates = parsed_response_address['result']['addressMatches'][0]['coordinates']
-lat = parsed_response_address['result']['addressMatches'][0]['coordinates']['y'] 
-lon = parsed_response_address['result']['addressMatches'][0]['coordinates']['x']
-
-## Lat/Lon to Route
-hike_url = f"https://www.hikingproject.com/data/get-trails?lat={lat}&lon={lon}&minStars=3&sort=quality&maxDistance={distance}&maxResults=500&key={hike_key}"
-response_hike = requests.get(hike_url)
-parsed_response_hike = json.loads(response_hike.text)
-hike_list = (parsed_response_hike)['trails']
-hike_name = (parsed_response_hike)['trails'][0]['name']
-hike_summary = (parsed_response_hike)['trails'][0]['summary']
-hike_length = (parsed_response_hike)['trails'][0]['length']
-hike_rating = (parsed_response_hike)['trails'][0]['stars']
-hike_difficulty_raw = (parsed_response_hike)['trails'][0]['difficulty']
-hike_location = (parsed_response_hike)['trails'][0]['location']
-hike_url_response = (parsed_response_hike)['trails'][0]['url']
-hike_id = (parsed_response_hike)['trails'][0]["id"]
-
-
-if hike_difficulty_raw == "green":
-    hike_difficulty = "Easy"
-elif hike_difficulty_raw == "greenBlue":
-    hike_difficulty = "Moderately Easy"    
-elif hike_difficulty_raw == "blue":
-    hike_difficulty = "Moderate"
-elif hike_difficulty_raw == "blueBlack":
-    hike_difficulty = "Moderately Hard"
-else:
-    hike_difficulty = "Hard"
-
-green_list = []
-greenBlue_list=[]
-blue_list = []
-blueBlack_list = []
-black_list = []
-
-
-for x in hike_list:
-    if str(x["difficulty"]) == "green":
-        blueBlack_list.append(x["id"]) 
-    elif str(x["difficulty"]) == "greenBlue":
-        greenBlue_list.append(x["id"]) 
-    elif str(x["difficulty"]) == "blue":
-        blue_list.append(x["id"]) 
-    elif str(x["difficulty"]) == "blueBlack":
-        blueBlack_list.append(x["id"]) 
+    while (parsed_response_hike)['success'] == int(1):
+        pass
+        break
     else:
-        black_list.append(x["id"]) 
+        print("Welp, we hit a snag. I couldn't get a valid response back from the Hiking Project.  Let's try again.")      
+        break
 
-green_trails = []
-greenBlue_trails=[]
-blue_trails = []
-blueBlack_trails = []
-black_trails = []
-selected_trails = []
+    hike_list = (parsed_response_hike)['trails']
+    hike_name = (parsed_response_hike)['trails'][0]['name']
+    hike_summary = (parsed_response_hike)['trails'][0]['summary']
+    hike_length = (parsed_response_hike)['trails'][0]['length']
+    hike_rating = (parsed_response_hike)['trails'][0]['stars']
+    hike_difficulty_raw = (parsed_response_hike)['trails'][0]['difficulty']
+    hike_location = (parsed_response_hike)['trails'][0]['location']
+    hike_url_response = (parsed_response_hike)['trails'][0]['url']
+    hike_id = (parsed_response_hike)['trails'][0]["id"]
 
-for x in hike_list:
-    if str(x["id"]) in str(green_list):
-        green_trails.append(x)
-    elif str(x["id"]) in str(greenBlue_list):
-        greenBlue_trails.append(x)
-    elif str(x["id"]) in str(blue_list):
-        blue_trails.append(x)
-    elif str(x["id"]) in str(blueBlack_list):
-        blueBlack_trails.append(x)
+
+    if hike_difficulty_raw == "green":
+        hike_difficulty = "Easy"
+    elif hike_difficulty_raw == "greenBlue":
+        hike_difficulty = "Moderately Easy"    
+    elif hike_difficulty_raw == "blue":
+        hike_difficulty = "Moderate"
+    elif hike_difficulty_raw == "blueBlack":
+        hike_difficulty = "Moderately Hard"
     else:
-        black_trails.append(x)
+        hike_difficulty = "Hard"
+
+    green_list = []
+    greenBlue_list=[]
+    blue_list = []
+    blueBlack_list = []
+    black_list = []
 
 
-if how_hard == str(1):
-    selected_trails = green_trails
-elif how_hard == str(2):
-    selected_trails = greenBlue_trails
-elif how_hard == str(3):
-    selected_trails = blue_trails
-elif how_hard == str(4):
-    selected_trails = blueBlack_trails
-elif how_hard == str(5):
-    selected_trails = black_trails
-else:
-    pass
+    for x in hike_list:
+        if str(x["difficulty"]) == "green":
+            blueBlack_list.append(x["id"]) 
+        elif str(x["difficulty"]) == "greenBlue":
+            greenBlue_list.append(x["id"]) 
+        elif str(x["difficulty"]) == "blue":
+            blue_list.append(x["id"]) 
+        elif str(x["difficulty"]) == "blueBlack":
+            blueBlack_list.append(x["id"]) 
+        else:
+            black_list.append(x["id"]) 
+
+    green_trails = []
+    greenBlue_trails=[]
+    blue_trails = []
+    blueBlack_trails = []
+    black_trails = []
+    selected_trails = []
+
+    for x in hike_list:
+        if str(x["id"]) in str(green_list):
+            green_trails.append(x)
+        elif str(x["id"]) in str(greenBlue_list):
+            greenBlue_trails.append(x)
+        elif str(x["id"]) in str(blue_list):
+            blue_trails.append(x)
+        elif str(x["id"]) in str(blueBlack_list):
+            blueBlack_trails.append(x)
+        else:
+            black_trails.append(x)
 
 
-selected_hike_name = selected_trails[0]['name']
-selected_hike_summary = selected_trails[0]['summary']
-selected_hike_length = selected_trails[0]['length']
-selected_hike_rating = selected_trails[0]['stars']
-selected_hike_difficulty_raw = selected_trails[0]['difficulty']
-selected_hike_location = selected_trails[0]['location']
-selected_hike_url_response = selected_trails[0]['url']
-selected_hike_id = selected_trails[0]["id"]
-selected_hike_difficulty = selected_trails[0]["difficulty"]
+    if how_hard == str(1):
+        selected_trails = green_trails
+    elif how_hard == str(2):
+        selected_trails = greenBlue_trails
+    elif how_hard == str(3):
+        selected_trails = blue_trails
+    elif how_hard == str(4):
+        selected_trails = blueBlack_trails
+    elif how_hard == str(5):
+        selected_trails = black_trails
+    else:
+        pass
 
 
+    if selected_trails != []:
+        selected_hike_name = selected_trails[0]['name']
+        selected_hike_summary = selected_trails[0]['summary']
+        selected_hike_length = selected_trails[0]['length']
+        selected_hike_rating = selected_trails[0]['stars']
+        selected_hike_difficulty_raw = selected_trails[0]['difficulty']
+        selected_hike_location = selected_trails[0]['location']
+        selected_hike_url_response = selected_trails[0]['url']
+        selected_hike_id = selected_trails[0]["id"]
+        selected_hike_difficulty = selected_trails[0]["difficulty"]
 
-print(f"There's {len(selected_trails)} trails that meet your criteria!")
-print("--------------------")
-print(f"We found a great hike for you! The highest rated '{how_hard}' difficutly route within {distance} miles is named '{selected_hike_name}'.")
-print(f"The route is {selected_hike_length} miles long and is located in {selected_hike_location}.")
-print(f"The community has given this trail {selected_hike_rating} out of 5 stars.")
-print(f"Here's a summary of the trail: {selected_hike_summary}")
-print(f"You can check out more about this hike at {selected_hike_url_response}.")
 
+        print(f"There's {len(selected_trails)} trails that meet your criteria!")
+        print("--------------------")
+        print("")
+        print(f"We found a great hike for you! The highest rated '{how_hard}' difficutly route within {distance} miles is named '{selected_hike_name}'.")
+        print(f"The route is {selected_hike_length} miles long and is located in {selected_hike_location}.")
+        print(f"The community has given this trail {selected_hike_rating} out of 5 stars.")
+        print(f"Here's a summary of the trail: {selected_hike_summary}")
+        print(f"You can check out more about this hike at {selected_hike_url_response}.")
+        break
+    else:
+        print("")
+        print("Welp, we hit a snag. I couldn't find anything that matched the difficulty and search radius that you provided.  You should try again, but maybe try selecting a different difficulty level or increasing your search radius.  Thanks!")
+        print("")
 
-
-
-breakpoint()
-
-# TODO: What is no trails?
